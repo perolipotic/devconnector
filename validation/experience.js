efa65632cf8b86731cf758aff5c09236582afe94
Pero <lipotic.pero@gmail.com>
2018-08-14 17:37:35 +0200

    finished all routs

--- validation/experience.js
+++ validation/experience.js
@@ -9,16 +9,19 @@ module.exports = function validateExperienceInput(data) {
   data.from = !isEmpty(data.from) ? data.from : "";
 
   if (validator.isEmpty(data.title)) {
-    errors.title = "Job title field is req";
+    errors.title = "Job title field is required";
   }
+
   if (validator.isEmpty(data.company)) {
-    errors.company = "Company  field is req";
+    errors.company = "Company field is required";
   }
+
   if (validator.isEmpty(data.from)) {
-    errors.from = "From date  field is req";
+    errors.from = "From date field is required";
   }
+
   return {
-    errors: errors,
+    errors,
     isValid: isEmpty(errors)
   };
 };
